f550eab7609c60304421aaca9a203edb9af02b75
neutmute <neutmute@users.noreply.github.com>
2015-07-24 10:42:31 +1000

    seed

---
+++ src/steelcap/Ace/WidgetBoxHelper.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.AspNet.Mvc.Rendering;
+//using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+//namespace Steelcap
+//{
+//    [TargetElement("widget-box")]
+//    public class WidgetBoxHelper : TagHelper
+//    {
+//        public string Title { get; set; }
+
+//        public override async void Process(TagHelperContext context, TagHelperOutput output)
+//        {
+//            output.TagName = "div";
+//            output.Attributes["class"] = "widget-box";
+
+//            var widgetHeaderDiv = new TagBuilder("div");
+//            widgetHeaderDiv.AddCssClass("widget-header");
+//            var h4 = new TagBuilder("h4");
+//            h4.AddCssClass("widget-title");
+//            h4.SetInnerText(Title);
+
+//            widgetHeaderDiv.InnerHtml += h4;
+
+//            var bodyDiv = new TagBuilder("div");
+//            bodyDiv.AddCssClass("widget-body");
+
+//            var widgetMain = new TagBuilder("div");
+//            widgetMain.AddCssClass("widget-main");
+
+//            var originalContent = await context.GetChildContentAsync();
+
+//            widgetMain.InnerHtml = originalContent.GetContent();
+//            bodyDiv.InnerHtml = widgetMain.ToHtmlString(TagRenderMode.Normal).ToString();
+
+//            var finalHtml = widgetHeaderDiv.ToHtmlString(TagRenderMode.Normal).ToString()
+//                            + bodyDiv.ToHtmlString(TagRenderMode.Normal).ToString();
+
+//            output.Content.SetContent(finalHtml);
+
+//            base.Process(context, output);
+//        }
+//    }
+//}
