2d7254ddea0f29219b5ec748b8842aa1de9e8aec
neutmute <neutmute@users.noreply.github.com>
2015-08-18 14:29:59 +1000

    extension methods

---
+++ src/SteelCap/Extensions/TagHelperExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace SteelCap.Extensions
+{
+    public static class HtmlStringExtensions
+    {
+        public static HtmlString Concat(this HtmlString first, string html)
+        {
+            return Concat(first, new HtmlString(html));
+        }
+
+        public static HtmlString Concat(this HtmlString first, params HtmlString[] htmlStringsForConcat)
+        {
+            var sb = new StringBuilder();
+            sb.Append(first);
+            foreach (var htmlString in htmlStringsForConcat)
+            {
+                sb.Append(htmlString);
+            }
+            return new HtmlString(sb.ToString());
+        }
+    }
+
+    public static class TagHelperExtensions
+    {
+        internal static void AppendClass(this TagHelperOutput target, string cssClass)
+        {
+            if (target.Attributes["class"] == null)
+            {
+                target.Attributes["class"] = string.Empty;
+            }
+            else
+            {
+                target.Attributes["class"].Value += " ";
+            }
+            target.Attributes["class"].Value += cssClass;
+        }
+    }
+}
