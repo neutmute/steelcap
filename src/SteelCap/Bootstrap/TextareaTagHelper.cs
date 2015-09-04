dd4f84f7eca682a89037545972924e0ae3c895f9
cormacdebarra <cdebarra@bauer-media.com.au>
2015-09-04 10:19:31 +1000

    Added a textarea tag helper

---
+++ src/SteelCap/Bootstrap/TextareaTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using SteelCap.Extensions;
+
+namespace SteelCap.Bootstrap
+{
+    [TargetElement("sc-textarea")]
+    public class TextareaTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "textarea";
+
+            output.AppendClass("form-control");
+
+            base.Process(context, output);
+        }
+
+        internal static void AppendClass(TagHelperOutput output, string cssClass)
+        {
+
+            if (output.Attributes["class"] == null)
+            {
+                output.Attributes["class"] = string.Empty;
+            }
+            output.Attributes["class"].Value += " " + cssClass;
+        }
+    }
+}
