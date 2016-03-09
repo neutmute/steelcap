88a7b77aca3048969bbd33da67ad2defa12588c4
neutmute <neutmute@users.noreply.github.com>
2016-03-10 10:54:14 +1100

    setup for team city assembly info stamping

--- src/SteelCap/Properties/AssemblyInfo.cs
+++ src/SteelCap/Properties/AssemblyInfo.cs
@@ -1,4 +1,19 @@
 ﻿
 using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SteelCap.Test")]
+
+
+#if DEBUG
+    [assembly: AssemblyProduct("Steelcap (Debug)")]
+    [assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyProduct("Steelcap (Release)")]
+    [assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("Developer Build")]
